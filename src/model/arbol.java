e8ed059ec0b8d9f14f39e58c48ff72acf0352fbd
Sergio <sramos02@ucm.es>
2020-04-23 09:58:00 +0200

    Clase arbol updated

--- src/model/arbol.java
+++ src/model/arbol.java
@@ -5,31 +5,51 @@ public class arbol<T> {
 	private arbol<T> izq=null;
 	private arbol<T> der=null;
 	private arbol<T> cen=null;
+	int numNodos;
+	int profundidad;
 	
 	public arbol() {}
 	public arbol(T elemento) {
 		this.elemento=elemento;
+		numNodos = 1;
+		profundidad = 1;
 	}
 	public arbol(T elemento, arbol<T>izq) {
 		this.elemento=elemento;
 		this.izq=izq;
+		numNodos = izq.numNodos++;
+		profundidad = izq.profundidad++;
 	}
+	
 	public arbol(T elemento, arbol<T>izq, arbol<T>der) {
 		this.elemento=elemento;
 		this.izq=izq;
 		this.der=der;
+		numNodos = izq.numNodos + der.numNodos + 1;
+		if(izq.profundidad > der.profundidad)profundidad = izq.profundidad++;
+		else profundidad = der.profundidad++;
+		
 	}
 	public arbol(T elemento, arbol<T>izq, arbol<T>der, arbol<T>cen) {
 		this.elemento=elemento;
 		this.izq=izq;
 		this.der=der;
 		this.cen=cen;
+		numNodos = izq.numNodos + der.numNodos + cen.numNodos;
+		
+		int prof = der.profundidad;
+		if(izq.profundidad > der.profundidad)prof = izq.profundidad;
+		if(cen.profundidad > prof)prof = cen.profundidad++;
+		profundidad = prof;
 	}
+	
 	public arbol(arbol<T> old) {
 		elemento=old.getElemento();
 		izq=old.getIzq();
 		der=old.getDer();
 		cen=old.getCen();
+		profundidad = old.profundidad;
+		numNodos = old.numNodos;
 	}
 	
 	public arbol<T> getCen() {
@@ -48,19 +68,33 @@ public class arbol<T> {
 		return elemento;
 	}
 	
+	public int getProfundidad() {
+		return profundidad;
+	}
+	
+	public int numElem() {
+		return numNodos;
+	}
+	
 	public boolean empty() {
 		return elemento==null;
 	}
 	
 	public void setIzq(arbol<T> izq) {
 		this.izq=izq;
+		numNodos++;
+		//profundidad ??
 	}
 	
 	public void setDer(arbol<T> der) {
 		this.der=der;
+		//profundidad ??
+		numNodos++;
 	}
 	
 	public void setCen(arbol<T> cen) {
 		this.cen=cen;
+		numNodos++;
+		//profundidad ??
 	}
 }
