b5b5ca3e37bc5818f458f6ecb987eeb64e7571e2
Vicky <Vicky>
2020-04-24 17:31:07 +0200

    Auto stash before merge of "Vicky" and "origin/Sergio"

--- src/model/arbol.java
+++ src/model/arbol.java
@@ -1,50 +1,76 @@
 package model;
 
-public class arbol<T> {
-	private T elemento=null;
-	private arbol<T> izq=null;
-	private arbol<T> der=null;
-	private arbol<T> cen=null;
+public class arbol {
+	private element elemento=null;
+	private arbol izq=null;
+	private arbol der=null; 
+	private arbol cen=null;
+	private algoritmo algoritmo;
 	
 	public arbol() {}
-	public arbol(T elemento) {
+	public arbol(element elemento) {
 		this.elemento=elemento;
 	}
-	public arbol(T elemento, arbol<T>izq) {
+	public arbol(element elemento, arbol izq) {
 		this.elemento=elemento;
 		this.izq=izq;
 	}
-	public arbol(T elemento, arbol<T>izq, arbol<T>der) {
+	public arbol(element elemento, arbol izq, arbol der) {
 		this.elemento=elemento;
 		this.izq=izq;
 		this.der=der;
 	}
-	public arbol(T elemento, arbol<T>izq, arbol<T>der, arbol<T>cen) {
+	public arbol(element elemento, arbol izq, arbol der, arbol cen) {
 		this.elemento=elemento;
 		this.izq=izq;
 		this.der=der;
 		this.cen=cen;
 	}
-	public arbol(arbol<T> old) {
+	public arbol(arbol old) {
 		elemento=old.getElemento();
 		izq=old.getIzq();
 		der=old.getDer();
 		cen=old.getCen();
 	}
 	
-	public arbol<T> getCen() {
+	/**Devuelve un nodo aleatorio del arbol del tipo indicado.
+	 * Valores del tipo:
+	 * 						-Nodo -> cualquier nodo del arbol.
+	 * 						-Funcion -> nodo función.
+	 * 						-Terminal -> nodo terminal.*/
+	arbol getNodoAleatorio(String tipo) {
+		arbol nodo=null;
+		int size=getNumeroNodos(tipo);
+		switch(tipo) {
+		case "Nodo":
+			break;
+		case "Funcion":
+			break;
+		case "Terminal":
+			break;
+		}
+		return nodo;
+	}
+	
+	/**Devuelve el número de nodos que hay del tipo indicado*/
+	private int getNumeroNodos(String tipo) {
+		int size=0;
+		
+		return size;
+	}
+	public arbol getCen() {
 		return cen;
 	}
 	
-	public arbol<T> getDer() {
+	public arbol getDer() {
 		return der;
 	}
 	
-	public arbol<T> getIzq() {
+	public arbol getIzq() {
 		return izq;
 	}
 	
-	public T getElemento() {
+	public element getElemento() {
 		return elemento;
 	}
 	
@@ -52,15 +78,16 @@ public class arbol<T> {
 		return elemento==null;
 	}
 	
-	public void setIzq(arbol<T> izq) {
+	public void setIzq(arbol izq) {
 		this.izq=izq;
 	}
 	
-	public void setDer(arbol<T> der) {
+	public void setDer(arbol der) {
 		this.der=der;
 	}
 	
-	public void setCen(arbol<T> cen) {
+	public void setCen(arbol cen) {
 		this.cen=cen;
 	}
+	
 }
